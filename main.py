16d4eb1f32cdfe903819722d14b86873bd81f6db
Juwan <jhollingsworth5@student.clayton.edu>
2022-10-07 17:55:23 -0400

    Implement MACD logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from AlgorithmImports import *
 
 
@@ -8,10 +9,10 @@ class Sergiobot(QCAlgorithm):
         self.SetStartDate(2013, 10, 7)  # Set Start Date
         self.SetEndDate(2013, 10, 11)  # Set End Date
         self.SetCash(100000)  # Set  Cash
-        self.AddEquity("SPY", Resolution.Minute)
+        self.AddEquity("NAS100", Resolution.Minute)
 
 
-     # OnData event is where our algorithm lives. New data points are pumped into this method.
+     # OnData event is where our algorithm lives. New data points are pumped into this f(x).
     def OnData(self, data):
 
           # define strategy here:
@@ -23,9 +24,19 @@ class Sergiobot(QCAlgorithm):
         # Eventually: Implement some form of sentiment analysis via Tensor flow or some ML package. Sergio will use this SA as a flag to determine if a trade is good or not. 
 
         # - MACD
+        # initialize the macd(12,26) w/ 9 day signal
+        self.macd = self.MACD("NAS100", 12, 26, 9, MovingAverageType.Exponential, Resolution.Daily)
+        # store datetime in var
+        self.previous = datetime.min
+        # Plot MACD Indicator + Signal 
+        self.PlotIndicator("MACD" , True, self.macd, self.macd.Signal )
+        # Plot NAS100, fast period, slow period
+        self.PlotIndicator("NAS100", self.macd.Fast, self.macd.Slow)
         # - RSI
         # - Stoch
         # - ATR 
+
+
 
         
         if not self.Portfolio.Invested:
